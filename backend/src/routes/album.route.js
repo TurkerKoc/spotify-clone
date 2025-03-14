70511575fcb361d0192e988321a7e6c07690c0e1
Turker Koc <39654393+TurkerKoc@users.noreply.github.com>
2025-03-14 22:59:25 +0100

    finalize backend

--- backend/src/routes/album.route.js
+++ backend/src/routes/album.route.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
+import { getAlbumById, getAllAlbums } from "../controllers/album.controller.js";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-    res.send("Hello World");
-});
+router.get("/", getAllAlbums);
+router.get("/:albumId", getAlbumById);
 
-export default router; 
+export default router;
