38ae50fdff5dd5031c396ed43a5473b995d3d641
Turker Koc <39654393+TurkerKoc@users.noreply.github.com>
2025-03-14 15:24:10 +0100

    frontend setup with tailwind shadcn clerk

---
+++ frontend/src/App.tsx
@@ -0,0 +1,24 @@
+import { SignInButton } from "@clerk/clerk-react"
+import { SignedOut } from "@clerk/clerk-react"
+import { UserButton } from "@clerk/clerk-react"
+import { SignedIn } from "@clerk/clerk-react"
+import { Button } from "./components/ui/button"
+
+function App() {
+  return (
+    <>
+    <header>
+      <SignedOut>
+        <SignInButton>
+          <Button variant={"outline"}>Sign In</Button>
+        </SignInButton>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
+    </>
+  )
+}
+
+export default App
