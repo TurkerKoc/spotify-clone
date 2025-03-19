f06fabbfb30db8f72eba1752cc6a6dbe229a343b
Turker Koc <39654393+TurkerKoc@users.noreply.github.com>
2025-03-19 16:43:32 +0100

    play on home page section grid

--- frontend/src/pages/home/components/SectionGrid.tsx
+++ frontend/src/pages/home/components/SectionGrid.tsx
@@ -1,6 +1,8 @@
 import { Song } from "@/types";
 import SectionGridSkeleton from "./SectionGridSkeletion";
 import { Button } from "@/components/ui/button";
+import PlayButton from "./PlayButton";
+
 type SectionGridProps = {
   title: string;
   songs: Song[];
@@ -20,16 +22,15 @@ function SectionGrid({ title, songs, isLoading }: SectionGridProps) {
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
         {songs.map((song) => (
           <div key={song._id} className="bg-zinc-800/40 p-4 rounded-md hover:bg-zinc-700/40 transition-all group cursor-pointer">
+            <div className="relative mb-4">
             <div className="aspect-square rounded-md shadow-lg overflow-hidden">
               <img src={song.imageUrl} alt={song.title} className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-105" />
-              {/* TODO: Add play button */}
             </div>
-            <div className="mt-4">
-              <h3 className="font-medium mb-2 truncate">{song.title}</h3>
-              <p className="text-sm text-zinc-400 truncate">{song.artist}</p>
+            <PlayButton song={song} />
             </div>
+            <h3 className="font-medium mb-2 truncate">{song.title}</h3>
+            <p className="text-sm text-zinc-400 truncate">{song.artist}</p>
           </div>
-
         ))}
       </div>
     </div>
